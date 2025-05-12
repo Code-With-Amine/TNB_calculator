b707f9ff6e21f66da95cb49082b52d79ca4ef995
AMINE ABAIDI <amineabaidi546@gmail.com>
2025-05-12 21:16:25 +0100

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { ModeToggle } from "./ui/toggole-mode";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+function Nav() {
+  return (
+    <div className="flex flex-wrap gap-6 items-center">
+      <Avatar>
+        <AvatarImage src="./TNBLogo.png" />
+        <AvatarFallback>TNB Logo</AvatarFallback>
+      </Avatar>
+
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>Taxe sur les terrains non bâtis</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>terrains Exo</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>terrains non Exo</MenubarItem>
+            <MenubarSeparator />
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Autre Tax</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>Taxi</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Debut de boissoin</MenubarItem>
+            <MenubarSeparator />
+          </MenubarContent>
+        </MenubarMenu>
+                <MenubarMenu>
+          <MenubarTrigger>Doc</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>Comment calculer tax urbain</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Debut de boissoin</MenubarItem>
+            <MenubarSeparator />
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default Nav;
