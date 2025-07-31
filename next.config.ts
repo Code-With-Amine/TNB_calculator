29ae74131a49ce1d13c950a9b87a9c311f238986
ramofr <commune202401@gmail.com>
2025-07-31 11:18:19 +0100

    make the app pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  // Any other Next.js config options here
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
